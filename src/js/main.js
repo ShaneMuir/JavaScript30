e333785c295e77963dcd81d2def4dd1752bf88b5
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-10-11 11:50:53 +0000

    Made 06 today and added a UI to the projects

---
+++ src/js/main.js
@@ -0,0 +1,158 @@
+'use strict';
+
+$(document).ready( function() {	
+
+    // HOME PAGE HEIGHT
+    jQuery(window).load(function() {
+            // will first fade out the loading animation
+        jQuery(".loader").fadeOut();
+            // will fade out the whole DIV that covers the website.
+        jQuery(".preloader").delay(1000).fadeOut("slow");
+    });
+
+     
+    // HOME PAGE HEIGHT
+    if ($('.home, .portfolio-hero').length) {
+        function fullhome() {
+            var hometext = $('.home, .portfolio-hero')            
+//            var homett = $('.hero-title').offset();
+//            $('.social').css('margin-top', homett.top)            
+            hometext.css({
+                "height": $(window).height() + "px"
+            });
+        }
+        fullhome();
+        $(window).resize(fullhome);
+    }
+        
+
+
+
+    // PORTFOLIO ISOTOPE
+    if ($('.isotope_items').length) {
+         var $container = $('.isotope_items');
+         $container.isotope();
+
+        $('.portfolio-filter ul li').on("click", function(){
+            $(".portfolio-filter ul li").removeClass("select-cat");
+            $(this).addClass("select-cat");              
+            var selector = $(this).attr('data-filter');
+            $(".isotope_items").isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+        });
+            return false;
+        });      
+    }
+
+
+    // PORTFOLIO EFFECT
+    $(".cbp-item").hover3d({
+        selector: "figure",
+        perspective: 3000,
+        shine: true
+    });
+
+    
+    $('.site-btn').click(function(){
+         $('.isotope_items').load('port.html').fadeIn();
+     });
+
+
+// PORTFOLIO CONTENT  
+    $('#grid-container').cubeportfolio({
+        layoutMode: 'grid',
+        filters: '.portfolio-filter',
+        gridAdjustment: 'responsive',
+        animationType: 'skew',
+        defaultFilter: '*',
+        gapVertical: 30,
+        gapHorizontal: 30,
+        singlePageAnimation: 'fade',
+        mediaQueries: [{
+                width: 700,
+                cols: 3,
+            }, {
+                width: 480,
+                cols: 2,
+                options: {
+                    caption: '',
+                    gapHorizontal: 30,
+                    gapVertical: 20,
+                }
+            }, {
+                width: 320,
+                cols: 1,
+                options: {
+                    caption: '',
+                    gapHorizontal: 50,
+                }
+            }],            
+        singlePageCallback: function (url, element) {
+            var t = this;
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 30000
+                })
+                .done(function (result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function () {
+                    t.updateSinglePage('AJAX Error! Please refresh the page!');
+                });
+        },
+            plugins: {
+                loadMore: {
+                    element: '#port-loadMore',
+                    action: 'click',
+                    loadItems: 2,
+                }
+            }
+    }); 
+
+
+
+
+}); // document ready end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
